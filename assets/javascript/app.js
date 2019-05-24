9b62e6f2b0809cc7bcdef6a5a416d4ee551457e8
Jebbla <jlp2722@gmail.com>
2019-05-24 12:52:03 -0500

    firebase added

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+var config = 
+{
+    apiKey: "AIzaSyDldQc5kJXoZ2Xw1PLXJwHSou-jLhKQjjc",
+      authDomain: "jebbla-train-sceduler.firebaseapp.com",
+      databaseURL: "https://jebbla-train-sceduler.firebaseio.com",
+      projectId: "jebbla-train-sceduler",
+      storageBucket: "jebbla-train-sceduler.appspot.com",
+      messagingSenderId: "797932428455",
+      appId: "1:797932428455:web:69114ee6b33841ea"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
